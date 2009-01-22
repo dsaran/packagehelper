ace63397e9249bffb99856c927db0858b065d24d
Daniel Saran <danielsaran@gmail.com>
2009-01-22 04:10:42 +0000

    Removing pyc files
    Added new module responsible for parsing file paths.
    Added test cases for new parsers module.
    
    Changed File object to implement __str__ and __eq__

---
+++ PackageHelper/package/types/parsers.py
@@ -0,0 +1,87 @@
+import re
+from package.domain.file import File
+from package.domain.database import Database
+
+SEP = "[/\\\\]"
+ANYTHING = ".*"
+EOL = "$"
+DATABASE="(?P<database>\\w+)"
+USER="(?P<user>\\w+)"
+TYPE="(?P<type>\\w+)"
+FILENAME="(?P<filename>.*)"
+class Parser:
+    def parser(self, path):
+        return None
+
+class RegexParser(Parser):
+    def __init__(self, regex):
+        self.regex = regex
+
+    def parse(self, path):
+        pattern = re.compile(self.regex)
+        match = pattern.match(path)
+        map = match.groupdict()
+
+        file = File()
+        database = Database(map['database'].upper(), map['user'].upper())
+        file.set_type(map['type'].upper())
+        file.set_database(database)
+        file.set_name(map['filename'])
+        return file
+
+
+class DefaultParser(Parser):
+    """Parses the file path using the following rule:
+    ANYTHING/<database>/<user>/<type>/<filename>
+    and
+    ANYTHING\<database>\<user>\<type>\<filename>"""
+
+    def parse(self, path):
+        regex = ANYTHING + SEP + "(?P<database>\\w+)" + SEP + "(?P<user>\\w+)" + SEP + "(?P<type>\\w+)" + SEP + "(?P<filename>.*)" + EOL
+        pattern = re.compile(regex)
+        match = pattern.match(path)
+        map = match.groupdict()
+
+        file = File()
+        database = Database(map['database'].upper(), map['user'].upper())
+        file.set_type(map['type'].upper())
+        file.set_database(database)
+        file.set_name(map['filename'])
+        return file
+
+class CustomParser(Parser):
+    """CustomParser to be used to parse paths matching to a
+    custom user defined expression. It offers some language sugars
+    to help the user to write regular expressions more easily.
+    Defined variables:
+        #SEP: path separator ('/' or '\')
+        #ANYTHING: match to anything
+        #EOL: represents the end of path.
+        #DATABASE: will be used to get the database from path.
+        #USER: will be used to extract the database username from path.
+        #TYPE: will be used to extract the type from path.
+        #FILENAME: will be used to extract the filename."""
+
+    def __init__(self, userRegex):
+        self.userRegex = userRegex
+        regex = userRegex.replace("#SEP", SEP)
+        regex = regex.replace("#ANYTHING", ANYTHING)
+        regex = regex.replace("#EOL", EOL)
+        regex = regex.replace("#DATABASE", DATABASE)
+        regex = regex.replace("#USER", USER)
+        regex = regex.replace("#TYPE", TYPE)
+        regex = regex.replace("#FILENAME", FILENAME)
+        self.regex = regex
+
+    def parse(self, path):
+        pattern = re.compile(self.regex)
+        match = pattern.match(path)
+        map = match.groupdict()
+
+        file = File()
+        database = Database(map['database'], map['user'])
+        file.set_type(map['type'])
+        file.set_database(database)
+        file.set_name(map['filename'])
+        return file
+
---
+++ PackageHelper/test/package/types/tc_parser.py
@@ -0,0 +1,54 @@
+from test.framework import TestCase
+#from test.mock import Mock
+from package.types.parsers import DefaultParser
+from package.domain.file import File
+from package.domain.database import Database
+
+class ParserTests(TestCase):
+
+    def setUp(self):
+        self.filename = "filename.sql"
+        self.database = "database_name"
+        self.user = "user_name"
+        self.type = "type_definition"
+
+        self.expectedFile = File()
+        self.expectedFile.set_name(self.filename)
+        self.expectedFile.set_type(self.type.upper())
+        expectedDatabase = Database(self.database.upper(), self.user.upper())
+        self.expectedFile.set_database(expectedDatabase)
+        
+
+    def testDefaultParser_DOS(self):
+        """DefaultParser should correctly parse dos-like paths.""" 
+        path = "C:\\DOCUMENTS AND SETTINGS\\PROJECT\\%s\\%s\\%s\\%s" % \
+                (self.database.upper(), self.user.upper(), \
+                 self.type.upper(), self.filename)
+
+        parser = DefaultParser()
+        file = parser.parse(path)
+
+        self.assertEquals(self.expectedFile, file, "File are not equal.")
+
+    def testDefaultParser_DOS_caseinsensitive(self):
+        """DefaultParser should correctly parse dos-like paths ignoring case."""
+        path = "C:\\DOCUMENTS AND SETTINGS\\PROJECT\\%s\\%s\\%s\\%s" % \
+                (self.database, self.user, self.type, self.filename)
+
+        parser = DefaultParser()
+        file = parser.parse(path)
+
+        self.assertEquals(self.expectedFile, file, "File are not equal.")
+
+
+    def testDefaultParser_UNIX(self):
+        """DefaultParser should parse correctly unix-like paths"""
+        path = "/HOME/USER/PROJECT/%s/%s/%s/%s" % \
+                (self.database.upper(), self.user.upper(), \
+                 self.type.upper(), self.filename)
+
+        parser = DefaultParser()
+        file = parser.parse(path)
+
+        self.assertEquals(self.expectedFile, file, "File are not equal.")
+
